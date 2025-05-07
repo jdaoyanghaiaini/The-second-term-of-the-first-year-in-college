6ff548f1c1825e340b7abd23777de25d25c09967
jdaoyanghaiaini <2145381080@qq.com>
2025-05-07 21:57:50 +0800

    指定字符串创建二叉树

---
+++ BinaryTree/src/createTree.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+class TreeNode{
+    char val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(char ch) {
+        this.val = ch;
+    }
+}
+// 注意类名必须为 Main, 不要有任何 package xxx 信息
+public class createTree {
+    public static int i = 0;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextLine()) { // 注意 while 处理多个 case
+            String str = in.nextLine();
+            TreeNode root = createTree(str);
+            inOrder(root);
+        }
+    }
+
+    public static TreeNode createTree(String str) {
+        TreeNode root = null;
+        char ch = str.charAt(i);
+        if(ch!='#') {
+            i++;
+            root = new TreeNode(ch);
+            root.left = createTree(str);
+            root.right = createTree(str);
+        }else {
+            i++;
+        }
+        return root;
+    }
+
+    public static void inOrder(TreeNode root) {
+        if(root == null)    return;
+        inOrder(root.left);
+        System.out.print(root.val+" ");
+        inOrder(root.right);
+    }
+}
