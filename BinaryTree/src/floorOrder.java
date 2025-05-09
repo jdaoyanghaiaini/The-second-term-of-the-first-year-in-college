2edb4d1312263c19a2c302994949ac5dc07becdd
jdaoyanghaiaini <2145381080@qq.com>
2025-05-09 22:25:22 +0800

    层序遍历二叉树

--- BinaryTree/src/floorOrder.java
+++ BinaryTree/src/floorOrder.java
@@ -1,16 +1,35 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class floorOrder {
+
 }
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<Integer> queue = new LinkedList<>();
-        List<List<Integer>> list = new ArrayList<>();
-        while(){
-
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> list = new LinkedList<>();
+        if(root == null)    return list;
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+            LinkedList<Integer> cur = new LinkedList<>();
+            int sz = queue.size();
+            while(sz!=0) {
+                TreeNode tmp = queue.peek();
+                if(tmp==null){
+                    sz--;
+                    continue;
+                }
+//                cur.add(tmp.val);
+                addNode(queue.poll(),queue);
+                sz--;
+            }
+            list.add(cur);
+            Collections.reverse(list);
         }
+    }
+    public Queue<TreeNode> addNode(TreeNode root,Queue<TreeNode> queue) {
+        if(root.left!=null) queue.offer(root.left);
+        if(root.right!=null) queue.offer(root.right);
+
+        return queue;
     }
 }
