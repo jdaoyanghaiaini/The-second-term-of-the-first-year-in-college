4d35537f4420c1b3f8bc1b313d90369828cc0117
jdaoyanghaiaini <2145381080@qq.com>
2025-05-21 22:44:15 +0800

    binarySearchTree

--- binarySearchTree/src/BinaSearchTree.java
+++ binarySearchTree/src/BinaSearchTree.java
@@ -56,7 +56,62 @@ public class BinaSearchTree {
     }
 
     public boolean remove(int val) {
+        TreeNode cur = root;
+        TreeNode parent = null;
+        if(cur==null) {
+            return false;
+        }
+        while(cur!=null) {
+            if(cur.val>val) {
+                parent = cur;
+                cur = cur.left;
+            } else if (cur.val<val) {
+                parent = cur;
+                cur = cur.right;
+            }else {
+                removeElement(parent,cur);
+                return true;
+            }
+        }
+        return false;
+    }
 
+    private void removeElement(TreeNode parent, TreeNode cur) {
+        if(cur.left==null) {
+            if(cur==root) {
+                cur = cur.right;
+            }else {
+                if(parent.left==cur) {
+                    parent.left = cur.right;
+                }else {
+                    parent.right = cur.right;
+                }
+            }
+        } else if (cur.right==null) {
+            if(cur==root) {
+               cur = cur.left;
+            }else {
+                if(parent.left==cur) {
+                    parent.left = cur.left;
+                }else {
+                    parent.right = cur.left;
+                }
+            }
+        }else {
+            TreeNode target = cur.right;
+            TreeNode targetParent = cur;
+            while(target.left!=null) {
+                targetParent = target;
+                target = target.left;
+            }
+            cur.val = target.val;
+            if(target == targetParent.left) {
+                targetParent.left = target.right;
+            }
+            else {
+                targetParent.right = target.right;
+            }
+        }
     }
 
 }
